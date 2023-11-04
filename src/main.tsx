0e89c1d30d2aa731700bc6c4a9c90ed979148345
Maksim <fil.maxim13@gmail.com>
2023-11-04 22:28:32 +0300

    fix: add HashRouter, Vite vs Router

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ErrorBoundary } from './components/error-boundary';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </HashRouter>
+  </React.StrictMode>
 );
